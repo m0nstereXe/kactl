e3ce35d33be7e952afd0fb1e994470518a2d54d2
thomasyang18 <tomyangtt18@gmail.com>
2024-09-11 11:45:13 -0400

    hashing

--- content/strings/Hashing.h
+++ content/strings/Hashing.h
@@ -1,54 +1,34 @@
 /**
- * Author: Simon Lindholm
- * Date: 2015-03-15
- * License: CC0
- * Source: own work
- * Description: Self-explanatory methods for string hashing.
- * Status: stress-tested
+ * Author: among us 
+ * Date: now
+ * License: ???
+ * Source: ???
+ * Description: akif
+ * Status: akif
  */
-#pragma once
 
-// Arithmetic mod 2^64-1. 2x slower than mod 2^64 and more
-// code, but works on evil test data (e.g. Thue-Morse, where
-// ABBA... and BAAB... of length 2^10 hash the same mod 2^64).
-// "typedef ull H;" instead if you think test data is random,
-// or work mod 10^9+7 if the Birthday paradox is not a problem.
-typedef uint64_t ull;
-struct H {
-	ull x; H(ull x=0) : x(x) {}
-	H operator+(H o) { return x + o.x + (x + o.x < x); }
-	H operator-(H o) { return *this + ~o.x; }
-	H operator*(H o) { auto m = (__uint128_t)x * o.x;
-		return H((ull)m) + (ull)(m >> 64); }
-	ull get() const { return x + !~x; }
-	bool operator==(H o) const { return get() == o.get(); }
-	bool operator<(H o) const { return get() < o.get(); }
-};
-static const H C = (ll)1e11+3; // (order ~ 3e9; random also ok)
-
-struct HashInterval {
-	vector<H> ha, pw;
-	HashInterval(string& str) : ha(sz(str)+1), pw(ha) {
-		pw[0] = 1;
-		rep(i,0,sz(str))
-			ha[i+1] = ha[i] * C + str[i],
-			pw[i+1] = pw[i] * C;
-	}
-	H hashInterval(int a, int b) { // hash [a, b)
-		return ha[b] - ha[a] * pw[b - a];
-	}
-};
-
-vector<H> getHashes(string& str, int length) {
-	if (sz(str) < length) return {};
-	H h = 0, pw = 1;
-	rep(i,0,length)
-		h = h * C + str[i], pw = pw * C;
-	vector<H> ret = {h};
-	rep(i,length,sz(str)) {
-		ret.push_back(h = h * C + str[i] - pw * str[i-length]);
-	}
-	return ret;
+typedef pair<unsigned ll, ll> hsh;
+ 
+#define K first
+#define V second
+#define M 1000000321
+#define OP(x, y) constexpr hsh operator x (const hsh a, const hsh b) { return { a.K x b.K, (a.V y b.V + M) % M }; }
+OP(+, +) OP(*, *) OP(-, + M -)
+mt19937 gen(chrono::steady_clock::now().time_since_epoch().count());
+uniform_int_distribution<ll> dist(256, M - 1);
+ 
+ll ans[NN];
+hsh p[NN], h[NN];
+ 
+ 
+hsh sub_hash(ll l, ll len) {
+    return h[l+len] - p[len]*h[l];
 }
 
-H hashString(string& s){H h{}; for(char c:s) h=h*C+c;return h;}
+
+p[0] = { 1, 1 }, p[1] = { dist(gen) | 1, dist(gen) };
+
+for (ll i=0; i<s.size(); ++i) {
+    p[i+1] = p[i]*p[1];
+    h[i+1] = (h[i]*p[1] + hsh{s[i], s[i]});
+}
